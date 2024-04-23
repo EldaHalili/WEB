48f247d542523b718fc76c5c1e0ca7ee48e67e8f
Kisi0 <kismeteguri@gmail.com>
2024-04-23 22:37:14 +0200

    js

--- Nutricion/nutricione.js
+++ Nutricion/nutricione.js
@@ -1,4 +1,17 @@
-function scrollToFemije() {
-    var femijeSection = document.getElementById('Femije');
-    femijeSection.scrollIntoView({ behavior: 'smooth' });
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
