0cbf9663aa98fd825654e7e0c512cb7d9d73974a
Elda <halilielda24@gmail.com>
2024-04-25 23:09:49 +0200

    about us

--- homePage/script.js
+++ homePage/script.js
@@ -3,7 +3,7 @@ class parallaxTiltEffect {
 
     constructor({element, tiltEffect}) {
 
-        this.element = element;
+        this.element = element;// Elementi HTML mbi të cilin do të aplikohet efekti
         this.container = this.element.querySelector(".container");
         this.size = [300, 360];
         [this.w, this.h] = this.size;
@@ -11,14 +11,14 @@ class parallaxTiltEffect {
         this.tiltEffect = tiltEffect;
 
         this.mouseOnComponent = false;
-
+// Lidhja e metodave të klasës me instancën e saj
         this.handleMouseMove = this.handleMouseMove.bind(this);
         this.handleMouseEnter = this.handleMouseEnter.bind(this);
         this.handleMouseLeave = this.handleMouseLeave.bind(this);
         this.defaultStates = this.defaultStates.bind(this);
         this.setProperty = this.setProperty.bind(this);
         this.init = this.init.bind(this);
-
+ // Inicializimi i instancës
         this.init();
     }
 
@@ -35,10 +35,11 @@ class parallaxTiltEffect {
             X = (-(offsetX - (this.w / 2)) / 3) / 3;
             Y = ((offsetY - (this.h / 2)) / 3) / 3;
         }
+         // Vendosja e vlerave të transformimit CSS bazuar në pozicionin e mausit
 
         this.setProperty('--rY', X.toFixed(2));
         this.setProperty('--rX', Y.toFixed(2));
-
+ // Përditësimi i pozicionit për sfond bazuar në kërcimin e mausit
         this.setProperty('--bY', (80 - (X / 4).toFixed(2)) + '%');
         this.setProperty('--bX', (50 - (Y / 4).toFixed(2)) + '%');
     }
@@ -74,7 +75,7 @@ class parallaxTiltEffect {
 }
 
 const $ = e => document.querySelector(e);
-
+// Krijimi i instancave të klasës parallaxTiltEffect për elementet e ndryshme
 const wrap1 = new parallaxTiltEffect({
     element: $('.wrap--1'),
     tiltEffect: 'reverse'
