08c02be358a8417d1d6ec29d36da4e542205577b
Kisi0 <kismeteguri@gmail.com>
2024-04-25 23:19:59 +0200

    done

--- Nutricion/nutricione.js
+++ Nutricion/nutricione.js
@@ -1,8 +1,3 @@
-// Vendos një event handler për ngjarjen e skrollit të dritares
-window.onscroll = function() {
-    scrollFunction()
-};
-
 // Kur përdoruesi klikon në buton, bëj skroll lart në fillimin e dokumentit
 function topFunction() {
     // Vendos pozicionin e skrollit të dokumentit dhe dritares së dokumentit në 0 për të shkuar në fillim
