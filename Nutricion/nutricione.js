c28ee058cf75d0b5a20f2ca49cac9c20f7811ed5
Kisi0 <kismeteguri@gmail.com>
2024-04-25 23:18:51 +0200

    done

--- Nutricion/nutricione.js
+++ Nutricion/nutricione.js
@@ -1,17 +1,11 @@
+// Vendos një event handler për ngjarjen e skrollit të dritares
 window.onscroll = function() {
     scrollFunction()
 };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
+// Kur përdoruesi klikon në buton, bëj skroll lart në fillimin e dokumentit
 function topFunction() {
+    // Vendos pozicionin e skrollit të dokumentit dhe dritares së dokumentit në 0 për të shkuar në fillim
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
